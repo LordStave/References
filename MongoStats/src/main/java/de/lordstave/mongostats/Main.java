8d2b1cd4d1b39fdb4d206172c89d69f06bf79ae8
LordStave <test123@unknown.com>
2017-12-13 23:18:36 +0100

    ~ Main.java

--- MongoStats/src/main/java/de/lordstave/mongostats/Main.java
+++ MongoStats/src/main/java/de/lordstave/mongostats/Main.java
@@ -5,11 +5,6 @@ import java.text.DecimalFormat;
 public class Main {
 
     public static void main(String[] args) {
-        double kdr_unmodified = 239213.234334;
-        test();
-    }
 
-    private static void test() {
-        System.out.println(100 > 987);
     }
 }
