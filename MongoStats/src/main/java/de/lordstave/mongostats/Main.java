0f41db6709d953a29687da2d67257ad951f88a28
LordStave <test123@unknown.com>
2017-12-13 21:24:17 +0100

    ~ Main.java + New method "test"

--- MongoStats/src/main/java/de/lordstave/mongostats/Main.java
+++ MongoStats/src/main/java/de/lordstave/mongostats/Main.java
@@ -4,5 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Kleiner Test :)");
+        test();
+    }
+
+    private static void test() {
+        System.out.println(100 > 987);
     }
 }
